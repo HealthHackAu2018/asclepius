d05e9e2da1736a43b5f0a7b8efbc35ce32678e12
Daniel Pedroso <daniel.exe@gmail.com>
2018-09-15 10:26:35 +1000

    ...

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
 
 export default class App extends React.Component {
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
