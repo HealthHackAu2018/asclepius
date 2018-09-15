cb3227c4c2d2816bfcf9b81b99a2a882d4835419
Daniel Pedroso <daniel.exe@gmail.com>
2018-09-15 18:28:08 +1000

    API/WEB

--- api/handler.js
+++ api/handler.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const Pusher = require('pusher');
 
 AWS.config.setPromisesDependency(Promise);
 AWS.config.update({region:'us-east-1'});
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: 'ap1',
+  encrypted: true,
+})
 
 module.exports.hello = async (event, context) => {
   return {
@@ -30,10 +38,40 @@ module.exports.pushDeviceData = async (event, context) => {
     Item: item
   }).promise();
 
+  console.log('Pushing');
+  await pusher.trigger('monitor', item.type, item);
+  console.log('pushed to monitor', item.type, item);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Ok',
+    }),
+  };
+};
+
+module.exports.getHeartRate = async (event, context) => {
+  const { Items: [ item = {} ] = [] } = await dynamodb.query({
+    TableName: process.env.DEVICE_DATA_TABLE,
+    Limit: 1,
+    KeyConditionExpression: '#tp = :tp',
+    ExpressionAttributeNames: {
+      '#tp': 'type',
+    },
+    ExpressionAttributeValues: {
+      ':tp': 'heartrate',
+    },
+    ScanIndexForward: false
+  }).promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      heartRate: item.data || 'unknown',
     }),
   };
 };
--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Pusher from 'pusher-js';
 import './App.css';
 
+const pusher = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
+  cluster: 'ap1',
+  forceTLS: true,
+});
+
 class App extends Component {
+  state = { heartRate: 'unknown' };
+
+  async componentDidMount() {
+    const { data } = await axios.get('https://l2qay1y8v5.execute-api.us-east-1.amazonaws.com/dev/heartrate');
+    this.setState({ heartRate: data.heartRate });
+    this.channel = pusher.subscribe('monitor');
+    this.channel.bind('heartrate', (evt) => {
+      this.setState({ heartRate: evt.data });
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Asclepius</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          BPM: {this.state.heartRate}
         </p>
       </div>
     );
